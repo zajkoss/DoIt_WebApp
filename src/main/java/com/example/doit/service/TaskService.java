0df8434062ebea9e93d360dff2ae99b41efa199b
Lukasz <zajkowskilukas@gmail.com>
2017-10-31 17:17:59 +0100

    Create Serice Interface , add methods

---
+++ src/main/java/com/example/doit/service/TaskService.java
@@ -0,0 +1,14 @@
+package com.example.doit.service;
+
+import com.example.doit.model.Task;
+
+import java.util.List;
+
+public interface TaskService {
+
+    public void addTask(Task task);
+    public void updateTask(Task task);
+    public void delete(long id);
+    public Task getTaskById(long id);
+    public List<Task> listTask();
+}
