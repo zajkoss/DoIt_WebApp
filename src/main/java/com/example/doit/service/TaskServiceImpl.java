8cd07e9574a2d9bc90818f22fbca9baecdce216f
Lukasz <zajkowskilukas@gmail.com>
2017-10-31 17:38:28 +0100

    Create Serivce, fill body method

---
+++ src/main/java/com/example/doit/service/TaskServiceImpl.java
@@ -0,0 +1,51 @@
+package com.example.doit.service;
+
+import com.example.doit.dao.TaskDao;
+import com.example.doit.dao.TaskDaoImpl;
+import com.example.doit.model.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class TaskServiceImpl implements TaskService {
+
+    private TaskDao taskDao;
+
+
+    @Autowired
+    public void setTaskDao(TaskDao taskDao) {
+        this.taskDao = taskDao;
+    }
+
+
+
+
+    @Override
+    public void addTask(Task task) {
+        this.taskDao.addTask(task);
+
+    }
+
+    @Override
+    public void updateTask(Task task) {
+        this.taskDao.updateTask(task);
+    }
+
+    @Override
+    public void delete(long id) {
+        this.taskDao.delete(id);
+    }
+
+    @Override
+    public Task getTaskById(long id) {
+        return taskDao.getTaskById(id);
+    }
+
+    @Override
+    public List<Task> listTask() {
+        return this.taskDao.listTask();
+    }
+}
